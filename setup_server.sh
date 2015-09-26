51f61406b3140e51af6c1595049b6722771bc08d
wtatsuru <wtatsuru@hatena.ne.jp>
2015-09-26 22:10:08 +0900

    basic setup

--- setup_server.sh
+++ setup_server.sh
@@ -2,3 +2,34 @@
 #
 # server initial setup script
 #
+
+apt update
+apt install sysstat htop strace percona-toolokit tcpdump
+
+# sysstat: sar
+echo 'ENABLED="true"' > /etc/default/sysstat
+service sysstat restart
+
+# sysctl parameters
+cat > /etc/sysctl.conf <<EOF
+net.ipv4.ip_local_port_range = 1024 65000
+
+net.core.somaxconn = 262144
+
+#net.ipv4.tcp_tw_reuse = 1
+#net.ipv4.tcp_fin_timeout = 3
+#net.ipv4.tcp_max_tw_buckets = 2000000
+
+#net.core.netdev_max_backlog = 8192
+EOF
+sysctl -p
+
+# ulimit
+cat > /etc/security/limits.conf <<EOF
+* soft nofile 40960
+* hard nofile 40960
+* soft stack 10240
+* hard stack 10240
+* soft nproc 65535
+* hard nproc 65535
+EOF
