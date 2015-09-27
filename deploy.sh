41807b34dd5dcb45772ac1235c8b6935aeba0dac
wtatsuru <wtatsuru@hatena.ne.jp>
2015-09-27 14:23:43 +0900

    clear logs on every deploy

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 set -x
 
-ssh -tt isucon02 'sudo su - isucon sh -c "cd ~/deploy && git pull && cd perl && ~/.local/perl/bin/carton install" && sudo systemctl restart isuxi.perl.service && ~isucon/slack.sh deployed'
+ssh -tt isucon02 'sudo su - isucon sh -c "cd ~/deploy && git pull && cd perl && ~/.local/perl/bin/carton install" && sudo systemctl restart isuxi.perl.service && sudo rm -v /var/log/nginx/* && sudo service nginx reload && ~isucon/slack.sh deployed'
