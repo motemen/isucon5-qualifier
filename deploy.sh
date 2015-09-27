fc49856a852189e05f80606e541810c4defbe0ae
motemen <motemen@gmail.com>
2015-09-27 10:58:48 +0900

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 set -x
 
-ssh -tt isucon02 'sudo su - isucon sh -c "cd ~/deploy && git pull && cd perl && ~/.local/perl/bin/carton install" && sudo systemctl restart isuxi.perl.service'
+ssh -tt isucon02 'sudo su - isucon sh -c "cd ~/deploy && git pull && cd perl && ~/.local/perl/bin/carton install" && sudo systemctl restart isuxi.perl.service && ~isucon/slack.sh deployed'
